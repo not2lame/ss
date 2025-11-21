ac1efd2bd31ed665a05b83114ca6d0f91da1adef
not2lame <gener311@gmail.com>
2025-11-20 21:49:51 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This is crucial for GitHub Pages. 
-  // It ensures that whether your site is at username.github.io or username.github.io/repo-name,
-  // the links will work correctly.
-  base: './', 
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  }
+  base: '/ss/', // This matches your repository name
 })
